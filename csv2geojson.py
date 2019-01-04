c16217dc46419db53db7576ae9070c11eec943ad
Igor Khomyakov <ikh@iri.columbia.edu>
2019-01-04 11:28:06 -0500

    smaple african stations

---
+++ csv2geojson.py
@@ -0,0 +1,34 @@
+import csv, json
+from geojson import Feature, FeatureCollection, Point
+
+def coord(s):
+   if s[-1:] in ['N','S','W','E']:
+      n = s[:-1]
+      d = s[-1:]
+   else:
+      n = s
+      d = ''
+   return float(n) * (1.0 if d in ['N','E',''] else -1.0) 
+
+features = []
+with open('africa.tsv') as csvfile:
+    reader = csv.reader(csvfile, delimiter='\t')
+    for code, country, elevation, latitude, longitude, station in reader:
+        latitude, longitude = map(coord, (latitude, longitude))
+        elevation = (None if elevation=="" else float(elevation))
+        features.append(
+            Feature(
+                geometry = Point((longitude, latitude)),
+                properties = {
+                    'countryCode': int(code),
+                    'country': country.capitalize(),
+                    'elevation': elevation,
+                    'station': station.capitalize(),
+                }
+            )
+        )
+
+collection = FeatureCollection(features)
+with open("africa.geojson", "w") as f:
+    f.write('%s' % collection)
+
